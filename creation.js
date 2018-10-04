392cd6de860423410c86892cd5dba52dab368536
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-10-04 12:01:10 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -1,1 +1,9 @@
-console.log('githubからtest');
+(function() {
+    "use strict";
+    kintone.events.on('app.record.detail.show',function(event){
+
+ var no = record["Table"].value[0].id
+console.log(no)
+      
+    }); 
+})();
