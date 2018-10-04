4d4a3e4f003007badc654d294d5ff0d3aba1a063
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-10-04 17:24:49 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -4,6 +4,21 @@
           
    console.log('テスト3');
    console.log(event);
+   
+   var record = event.record
+      
+   var postingdate = record.掲載切替日.value;
+   
+   event.record.掲載完了日.value = postingdate;
+   
+
+   
+      
+    kintone.events.on(['app.record.edit.show', 'app.record.create.show'], function (event) {
+    // 「掲載完了日」フィールドの入力を制限
+        event.record.掲載完了日.disabled = true;
+
+    return event;
      
      
      
