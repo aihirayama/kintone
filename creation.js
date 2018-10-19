6e71232abf0715b397e305e6a73034eaa35a95c0
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-10-19 11:04:34 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -104,7 +104,7 @@
       console.log('ok1');
       for (var i = 0; i < trailing_character.length; i++) {
          console.log('ok2');
-         for(var j = 0; j < industry_counter.lenght; j++) {
+         for(var j = 0; j < industry.lenght; j++) {
             console.log('ok3');
             record[industry[j] + trailing_character[i]].value = industry_counter[i][j];
             console.log('ok4');
