8a0514492d739294feddfb438b5f46f843a41030
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-10-30 11:15:55 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -41,9 +41,8 @@
        
        //顧客起因不備のステータスが解除済に変更されたとき、顧客起因待機解除日になにも入力されていなければ今日の日付を入力。
        var today = new Date();
-       console.log("record['顧客起因不備'].value:",record['顧客起因不備'].value);
-       
-       if(!record['顧客起因不備'].value) {
+      
+       if(!record['顧客起因待機解除日'].value) {
           console.log('ok1');
           if(record['顧客起因不備'].value === '解除済') {
              console.log('ok2')
@@ -52,7 +51,7 @@
        }
        
        //顧客起因不備のステータスが解除済に変更されたとき、顧客起因待機解除日になにも入力されていなければ今日の日付を入力。
-       if(!record['社内起因不備'].value) {
+       if(!record['社内起因待機解除日'].value) {
           if(record['社内起因不備'].value === '解除済') {
             record['社内起因待機解除日'].value = today;
           }
