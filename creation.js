2e2a5da937b4846274f67d41dc490eb3540481af
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-10-30 16:15:20 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -42,23 +42,23 @@
        //顧客起因不備のステータスが解除済に変更されたとき、顧客起因待機解除日になにも入力されていなければ今日の日付を入力。
        var dt = new Date();
        var date = dt.getFullYear()+'-'+ (dt.getMonth()+1)+'-'+ dt.getDate();
-      /* 
-       if(!record.顧客起因待機解除日.value) {
+
+       if(record.顧客起因待機解除日.value === "") {
           if(record.顧客起因不備.value === '解除済') {
             record.顧客起因待機解除日.value = date;
           }
        }
   
        //社内起因不備のステータスが解除済に変更されたとき、顧客起因待機解除日になにも入力されていなければ今日の日付を入力。
-       if(!record.社内起因待機解除日.value) {
+       if(record.社内起因待機解除日.value === "") {
           if(record.社内起因不備.value === '解除済') {
             record.社内起因待機解除日.value = date;
           }
        }
-     */
+
        //予定/履歴メモに何か入力されたとき、対応予定日と対応日になにも入力されていなければ対応日に今日の日付を入力。
-       if(!record.履歴メモ.value) {
-          if(!record.対応予定日.value || !record.対応日.value) {
+       if(record.履歴メモ.value) {
+          if(record.対応予定日.value === "" || record.対応日.value === "") {
             record.対応日.value = date;
           }
        }
