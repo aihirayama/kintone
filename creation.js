9a7a9da1e8123e77430fd6fd19f43c46f921b402
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-11-09 16:49:40 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -188,7 +188,7 @@
       var industryStatsCounter = new Array(industryStatsNameList.length).fill(new Array(industryList.length).fill(0))
 
       //依頼情報テーブルの中から業態ごとの「新規作成(掲載なし)','追加掲載(施設登録なし)','新規作成(掲載あり)」ステータスを集計する関数
-      function posting_counter (tableName,industryStatsName,facilityStyle){
+      /*function posting_counter (tableName,industryStatsName,facilityStyle){
           for( var i = 0; i < record[tableName].value.length; i++) {
             for( var j = 0; j < industryStatsNameList.length; j++){
               if(record[tableName].value[i].value[industryStatsName].value === industryStatsNameList[j]) {
@@ -200,10 +200,22 @@
               }
             }
           }
-       }
+       }*/
 
+      for( var i = 0; i < record['施設情報テーブル'].value.length; i++) {
+        for( var j = 0; j < industryStatsNameList.length; j++){
+          if(record['施設情報テーブル'].value[i].value['依頼ステータス_施設'].value === industryStatsNameList[j]) {
+            for( var k = 0; k < industryList.length; k++) {
+              if(record['施設情報テーブル'].value[i].value['施設形態_施設'].value === industryList[k]) {
+                industryStatsCounter[j][k] += 1;
+              }
+            }
+          }
+        }
+      }
+      
       //施設情報テーブルを集計
-      posting_counter('施設情報テーブル','依頼ステータス_施設','施設形態_施設');
+     //posting_counter('施設情報テーブル','依頼ステータス_施設','施設形態_施設');
 
       //求人情報テーブルを集計
      // posting_counter('求人情報テーブル','依頼ステータス_求人','施設形態_求人');
