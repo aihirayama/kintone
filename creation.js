ff12ab34ccf30307dbd141bec84b96d9b99e4bb8
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-11-09 10:10:33 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -4,7 +4,7 @@
    // レコード詳細画面が表示された時のイベント-------------------------------------------------------------------------- 
    kintone.events.on('app.record.detail.show',function(event){
       console.log(event);
-      var record = event.record
+      var record = event.record;
       
       //詳細画面「レコードを再利用する」を非表示
       document.getElementsByClassName('gaia-argoui-app-menu-copy')[0].style.display = 'none';
@@ -75,14 +75,14 @@
          '登録・掲載_合計'
       ];
       
-   var industryList = ['病院','診療所','歯科','代替','介護福祉','薬局','訪問看護','保育','その他']
+   var industryList = ['病院','診療所','歯科','代替','介護福祉','薬局','訪問看護','保育','その他'];
    
    industryList.forEach(function(item) {
-     items.push(item + '_登録のみ')
-     items.push(item + '_登録・掲載')
-     items2.push(item + '_掲載のみ')
-     items2.push(item + '_登録・掲載') 
-   })     
+     items.push(item + '_登録のみ');
+     items.push(item + '_登録・掲載');
+     items2.push(item + '_掲載のみ');
+     items2.push(item + '_登録・掲載');
+   });
     
     
    //原稿ありなし選択で「原稿なし」が選択されていたら求人情報テーブルを非表示にする。(-"-)うぅ
@@ -103,7 +103,7 @@
       var deficiencyStatus = {
          '顧客起因待機解除日' : '顧客起因不備',
          '社内起因待機解除日' : '社内起因不備'
-      }
+      };
       
       Object.keys(deficiencyStatus).forEach(function(item) {
          if(!record[item].value && record[deficiencyStatus[item]].value === '解除済') {
@@ -116,41 +116,40 @@
 
    // レコードが保存された時のイベント-------------------------------------------------------------------------- 
    kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], function (event){
-   var record = event.record
+   var record = event.record;
        
     //予定/履歴メモに何か入力されたとき、予定日時　or　完了日時になにも入力されていなければエラー。タスク担当者が入力されてなければエラー
-      var hearingTtable = record.ヒアリング履歴テーブル.value 
-      var errormessage = ""
+      var hearingTtable = record.ヒアリング履歴テーブル.value ;
+      var errormessage = "";
        for (i = 0; i < hearingTtable.length; i++) {
           if(hearingTtable[i].value.履歴メモ.value) {
              if(!hearingTtable[i].value.予定日時.value && !hearingTtable[i].value.完了日時.value) {
-                errormessage += "[ヒアリング予定/履歴] Next予定日時　or　完了日時"
+                errormessage += "[ヒアリング予定/履歴] Next予定日時　or　完了日時";
              } 
              if(!hearingTtable[i].value.タスク担当者.value[0]) {
-                errormessage += "[ヒアリング予定/履歴] タスク担当者"               
+                errormessage += "[ヒアリング予定/履歴] タスク担当者";        
              } 
           }
        }
       
       if(errormessage) { 
-         event.error = '以下の項目を入力してください。'
-            + errormessage;
+         event.error = '以下の項目を入力してください。' + errormessage;
       }
  
      //依頼内容の「施設作成件数」、「施設変更件数」、「施設削除件数」を数える。 
     
        var facilityStatsNameList = ['施設作成件数','施設変更件数','削除件数'];
        var facilityStatsCounter = new Array(facilityStatsNameList.length).fill(0);//facilityStatsNameList分の0の配列
-       var facilityTable = record.施設情報テーブル.value
+       var facilityTable = record.施設情報テーブル.value;
        
       if(record.施設登録ありなし選択.value.indexOf('施設登録あり') >= 0) {
          for(var i = 0; i < facilityTable.length; i++) {   
-            var facilityTable2 = facilityTable[i].value.依頼ステータス_施設.value
-            facilityStatsCounter[0] += facilityTable2.indexOf('新規作成(掲載あり)') + 1 
-            facilityStatsCounter[0] += facilityTable2.indexOf('新規作成(掲載なし)') + 1
-            facilityStatsCounter[1] += facilityTable2.indexOf('変更') + 1
-            facilityStatsCounter[2] += facilityTable2.indexOf('施設削除') + 1
-      };
+            var facilityTable2 = facilityTable[i].value.依頼ステータス_施設.value;
+            facilityStatsCounter[0] += facilityTable2.indexOf('新規作成(掲載あり)') + 1; 
+            facilityStatsCounter[0] += facilityTable2.indexOf('新規作成(掲載なし)') + 1;
+            facilityStatsCounter[1] += facilityTable2.indexOf('変更') + 1;
+            facilityStatsCounter[2] += facilityTable2.indexOf('施設削除') + 1;
+      }
       }
                                                           
        for(var i = 0; i < facilityStatsNameList.length; i++) {
@@ -161,17 +160,17 @@
       
          var jobofferStatsNameList = ['求人作成件数','求人変更件数','非掲載化・削除求人数'];
          var jobofferStatsCounter = new Array(jobofferStatsNameList.length).fill(0);//facilityStatsNameList分の0の配列
-         var jobofferTable = record.求人情報テーブル.value
+         var jobofferTable = record.求人情報テーブル.value;
          
       if(record.原稿ありなし選択.value.indexOf('原稿あり') >= 0) {
          for(var i = 0; i < jobofferTable.length; i++) {   
-            var jobofferTable2 = jobofferTable[i].value.依頼ステータス_求人.value
-            jobofferStatsCounter[0] += jobofferTable2.indexOf('新規作成(施設登録あり)') + 1 
-            jobofferStatsCounter[0] += jobofferTable2.indexOf('追加掲載(施設登録なし)') + 1
-            jobofferStatsCounter[1] += jobofferTable2.indexOf('既存修正') + 1
-            jobofferStatsCounter[2] += jobofferTable2.indexOf('応募受付終了処理・求人削除') + 1
+            var jobofferTable2 = jobofferTable[i].value.依頼ステータス_求人.value;
+            jobofferStatsCounter[0] += jobofferTable2.indexOf('新規作成(施設登録あり)') + 1 ;
+            jobofferStatsCounter[0] += jobofferTable2.indexOf('追加掲載(施設登録なし)') + 1;
+            jobofferStatsCounter[1] += jobofferTable2.indexOf('既存修正') + 1;
+            jobofferStatsCounter[2] += jobofferTable2.indexOf('応募受付終了処理・求人削除') + 1;
          }
-      };
+      }
                                                                
        for(var i = 0; i < jobofferStatsNameList.length; i++) {
          record[jobofferStatsNameList[i]].value = jobofferStatsCounter[i];
@@ -229,12 +228,12 @@
 
     //レコード編集画面が表示された時のイベント&レコード追加画面が表示された時のイベント------------------------------------- 
     kintone.events.on(['app.record.edit.show', 'app.record.create.show'], function (event) {
-    var record = event.record
+    var record = event.record;
     // フィールドの入力を制限
-    var fieldName = ['掲載完了日','施設作成件数','施設変更件数','削除件数','求人作成件数','求人変更件数','非掲載化・削除求人数']
+    var fieldName = ['掲載完了日','施設作成件数','施設変更件数','削除件数','求人作成件数','求人変更件数','非掲載化・削除求人数'];
      for (var i = 0; i < fieldName.length; i++ ) {
         record[fieldName[i]].disabled = true;
-     };
+     }
       
     //「業態_登録のみ」「業態_掲載のみ」「業態_登録・掲載」フィールドの入力を制限
     var industry = ['病院','診療所','歯科','代替','介護福祉','薬局','訪問看護','保育','その他'];
