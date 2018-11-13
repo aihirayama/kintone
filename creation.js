e69c4a0e182fc07b1997cc6324a65f8eb45d39d0
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-11-13 16:17:10 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -14,34 +14,40 @@
 
       　 //ログインユーザの情報を取得
        function addMemberMine() {
-         var loginuser = kintone.getLoginUser();
-         var member = event['record']['仮原稿送付担当者_進捗管理']['value'];
-         var objParam = {};
-         objParam['app'] = kintone.app.getId();       // アプリ番号
-         objParam['id'] = kintone.app.record.getId(); // レコード番号
-         objParam['record'] = {};
-         objParam['record']['仮原稿送付担当者_進捗管理'] = {};
-         objParam['record']['仮原稿送付担当者_進捗管理']['value'] = [];
- 
-         // すでに担当者になっているメンバーを追加する
-             for (var i = 0; i < member.length; i++) {
-                 objParam['record']['仮原稿送付担当者_進捗管理']['value'][i] = {};
-                 objParam['record']['仮原稿送付担当者_進捗管理']['value'][i]['code'] = {};
-                 objParam['record']['仮原稿送付担当者_進捗管理']['value'][i]['code'] = member[i]['code'];
-             }
 
-             //ログインユーザを追加する
-             objParam['record']['仮原稿送付担当者_進捗管理']['value'][member.length] = {};
-             objParam['record']['仮原稿送付担当者_進捗管理']['value'][member.length]['code'] = {};
-             objParam['record']['仮原稿送付担当者_進捗管理']['value'][member.length]['code'] = loginuser.code;
-         
-          
-          // レコードを更新する
-         kintone.api('/k/v1/record', 'PUT', objParam, function(resp) {
-         // 成功時は画面をリロード
-             location.reload(true);
-         });
-        }
+     //ログインユーザの情報を取得
+     var loginuser = kintone.getLoginUser();
+        var  member = event['record']['仮原稿送付担当者_進捗管理']['value'];
+
+
+     var objParam = {};
+     objParam['app'] = kintone.app.getId();       // アプリ番号
+     objParam['id'] = kintone.app.record.getId(); // レコード番号
+     objParam['record'] = {};
+     objParam['record']['仮原稿送付担当者_進捗管理'] = {};
+     objParam['record']['仮原稿送付担当者_進捗管理']['value'] = [];
+
+     // すでに担当者になっているメンバーを追加する
+     for (var i = 0; i < member.length; i++) {
+         objParam['record']['仮原稿送付担当者_進捗管理']['value'][i] = {};
+         objParam['record']['仮原稿送付担当者_進捗管理']['value'][i]['code'] = {};
+         objParam['record']['仮原稿送付担当者_進捗管理']['value'][i]['code'] = member[i]['code'];
+     }
+
+     //ログインユーザを追加する
+     objParam['record']['仮原稿送付担当者_進捗管理']['value'][member.length] = {};
+     objParam['record']['仮原稿送付担当者_進捗管理']['value'][member.length]['code'] = {};
+     objParam['record']['仮原稿送付担当者_進捗管理']['value'][member.length]['code'] = loginuser.code;
+
+     // レコードを更新する
+     kintone.api('/k/v1/record', 'PUT', objParam, function(resp) {
+     // 成功時は画面をリロード
+         location.reload(true);
+     });
+   }
+    Button.addEventListener('click', addMemberMine);
+ });
+  
 
         
     
