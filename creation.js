45665b218aa6d7db935ccbf1bba6e1140aac266b
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-11-13 12:02:57 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -246,14 +246,16 @@
 
 
 (function() {
-  "use strict";
+    "use strict";
  
-       // 任意のスペースフィールドにボタンを設置
-        var mySpaceFieldButton = document.createElement('button');
-        mySpaceFieldButton.id = 'my_space_field_button';
-        mySpaceFieldButton.innerHTML = 'スペースボタン';
-        mySpaceFieldButton.onclick = function () {
-            window.alert('スペースフィールド');
+    kintone.events.on('app.record.detail.show', function(event) {
+        // メニュ右側の空白部分にボタンを設置
+        var myIndexButton = document.createElement('button');
+        myIndexButton.id = 'my_index_button';
+        myIndexButton.innerHTML = 'メニュー部ボタン';
+        myIndexButton.onclick = function() {
+            window.alert('メニュー部');
         }
-        kintone.app.record.getSpaceElement('my_space_field').appendChild(mySpaceFieldButton);
+        kintone.app.record.getHeaderMenuSpaceElement().appendChild(myIndexButton);
+    });
 })();
