c860dddf13a33353142605a6c53607614b8fc9b2
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-11-13 12:07:23 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -36,6 +36,15 @@
       kintone.app.record.getFieldElement('顧客ID').appendChild(clienttmpA);
     }
     
+       // 任意のスペースフィールドにボタンを設置
+        var mySpaceFieldButton = document.createElement('button');
+        mySpaceFieldButton.id = 'my_space_field_button';
+        mySpaceFieldButton.innerHTML = 'スペースボタン';
+        mySpaceFieldButton.onclick = function () {
+            window.alert('スペースフィールド');
+        }
+        kintone.app.record.getSpaceElement('my_space_field').appendChild(mySpaceFieldButton);
+
 
   });
 
@@ -244,25 +253,5 @@
 
 })();
 
-(function() {
-  "use strict";
- 
-    kintone.events.on('app.record.detail.show', function(event) {
-        var myIndexButton = document.createElement('button');
-        myIndexButton.id = 'my_index_button';
-        myIndexButton.innerHTML = 'メニュー部ボタン';
-        myIndexButton.onclick = function () {
-            window.alert('メニュー部');
-        }
-        kintone.app.record.getHeaderMenuSpaceElement().appendChild(myIndexButton);
 
-        // 任意のスペースフィールドにボタンを設置
-        var mySpaceFieldButton = document.createElement('button');
-        mySpaceFieldButton.id = 'my_space_field_button';
-        mySpaceFieldButton.innerHTML = 'スペースボタン';
-        mySpaceFieldButton.onclick = function () {
-            window.alert('スペースフィールド');
-        }
-        kintone.app.record.getSpaceElement('my_space_field').appendChild(mySpaceFieldButton);
-    });
-})();
+     
