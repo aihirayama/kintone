8c11a8cffee294bd8ec241396b7faf78806d3324
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-11-13 12:05:32 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -244,18 +244,25 @@
 
 })();
 
-
 (function() {
-    "use strict";
+  "use strict";
  
     kintone.events.on('app.record.detail.show', function(event) {
-        // メニュ右側の空白部分にボタンを設置
         var myIndexButton = document.createElement('button');
         myIndexButton.id = 'my_index_button';
         myIndexButton.innerHTML = 'メニュー部ボタン';
-        myIndexButton.onclick = function() {
+        myIndexButton.onclick = function () {
             window.alert('メニュー部');
         }
         kintone.app.record.getHeaderMenuSpaceElement().appendChild(myIndexButton);
+
+        // 任意のスペースフィールドにボタンを設置
+        var mySpaceFieldButton = document.createElement('button');
+        mySpaceFieldButton.id = 'my_space_field_button';
+        mySpaceFieldButton.innerHTML = 'スペースボタン';
+        mySpaceFieldButton.onclick = function () {
+            window.alert('スペースフィールド');
+        }
+        kintone.app.record.getSpaceElement('my_space_field').appendChild(mySpaceFieldButton);
     });
 })();
