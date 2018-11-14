fa49edbbd88cdcc69b0a7d56f9cca1ec021d91eb
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-11-14 11:40:21 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -14,27 +14,25 @@
     kintone.app.record.getSpaceElement('my_space_field').appendChild(Button);
 
     //ログインユーザの情報を取得
-    function addMemberMine() {
+    function addMemberMine(x) {
       //ログインユーザの情報を取得
       var loginuser = kintone.getLoginUser();
-      var  member = record.仮原稿送付担当者_進捗管理.value;
+      var  member = record[x].value;
 
       var objParam = {};
       objParam.app = kintone.app.getId();       // アプリ番号
       objParam.id = kintone.app.record.getId(); // レコード番号
       objParam.record = {};
-      objParam.record.仮原稿送付担当者_進捗管理 = {};
-      objParam.record.仮原稿送付担当者_進捗管理.value = [];
+      objParam.record[x] = {};
+      objParam.record[x].value = [];
 
       // すでに担当者になっているメンバーを追加する
       for (var i = 0; i < member.length; i++) {
-        objParam.member[i] = {'code': member[i].code};
-       // objParam.record.仮原稿送付担当者_進捗管理.value[i] = {'code': member[i].code};
+       objParam.record[x].value[i] = {'code': member[i].code};
       }
 
       //ログインユーザを追加する
-      objParam.member[member.length] = {'code': loginuser.code};
-      //objParam.record.仮原稿送付担当者_進捗管理.value[member.length] = {'code': loginuser.code};
+      objParam.record[x].value[member.length] = {'code': loginuser.code};
 
       // レコードを更新する
       kintone.api('/k/v1/record', 'PUT', objParam, function(resp) {
@@ -42,7 +40,7 @@
         location.reload(true);
       });
     }
-    Button.addEventListener('click', addMemberMine);
+    Button.addEventListener('click', addMemberMine('仮原稿送付担当者_進捗管理'));
 
 
     //詳細画面「レコードを再利用する」を非表示
