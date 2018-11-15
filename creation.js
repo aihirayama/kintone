60fa3d5774b9ee3da943ad3ba3ae6ce00ec1f127
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-11-15 10:40:05 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -1,6 +1,6 @@
 (function() {
   "use strict";
-  //neko
+  //tesutochuuuuuuuuuuuuuuuuu
   
 　//レコード詳細画面が表示された時のイベント-------------------------------------------------------------------------- 
   kintone.events.on('app.record.detail.show', function(event) {
@@ -193,9 +193,11 @@
 
     for(var i = 0; i < tbr_f.length; i++) {
       if(record.施設登録ありなし選択.value === '施設登録あり') {
+        console.log('施設登録ありなし選択のvalue',record.施設登録ありなし選択.value);
         for(var j = 0; j < errorlist_f.length j++) {
+          console.log('tbr_f[i].value[errorlist_f[j]].value',tbr_f[i].value[errorlist_f[j]].value);
           if(tbr_f[i].value[errorlist_f[j]].value){
-              tbr_f[i].value[errorlist_f[j]].error = 'なんで入力しないの？やる気あんのかよ！！！！'
+              tbr_f[i].value[errorlist_f[j]].error = '入力して下さい。';
              }
         }      
       }
