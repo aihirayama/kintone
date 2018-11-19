8f485c74b789c5fd9d88d94e568da7b91d26d8b9
aihirayama <43776901+aihirayama@users.noreply.github.com>
2018-11-19 11:35:07 +0900

    Update creation.js

--- creation.js
+++ creation.js
@@ -1,6 +1,6 @@
 (function() {
   "use strict";
-  //🐺🐺🐺↑↑↑↑↑↑↑↑↑↑↑
+  //🐺🐺🐺--------------🏥👐
   
 //レコード詳細画面が表示された時のイベント-------------------------------------------------------------------------- 
   kintone.events.on('app.record.detail.show', function(event) {   
@@ -331,7 +331,7 @@
   
 ///-------------test🐏↓↓↓↓↓↓
 //レコード一覧画面を保存する時のイベント
-  kintone.events.on('app.record.index.edit.submit', function (event){
+  kintone.events.on(['app.record.index.edit.submit','app.record.index.show'], function (event){
     var records = event.records;
     var dt = new Date();
     var date = dt.getFullYear()+'-'+ (dt.getMonth()+1)+'-'+ dt.getDate();
